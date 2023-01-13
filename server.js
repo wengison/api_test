f66d8ae736e9410b211332556b69fe4c32d9e591
wengison <venglar01@gmail.com>
2023-01-13 10:28:02 +0100

    cors mod

--- server.js
+++ server.js
@@ -14,13 +14,19 @@ const app = express()
 //midlleware 
 app.use(express.json()) //-> 'req.body'
 
+app.use((req, res, next)=> {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
+
 app.use('/api/todos', routes)
 
 const connectToMongo = async () => {
     try {
       await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
       console.log('connected to MongoDB');
-      app.listen(process.env.PORT||3000, ()=> console.log('Server running'))
+      app.listen(process.env.PORT||3000, ()=> console.log('Server running & it is public'))
     } catch(error) {
       console.log('error connection to MongoDB:', error.message);
     }
