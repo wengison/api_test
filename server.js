22d865c07f400ad2826d68c8c31f45139c47fc55
wengison <venglar01@gmail.com>
2023-01-14 00:19:40 +0100

    mod

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const connectToMongo = async () => {
     try {
       await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
       console.log('connected to MongoDB');
-      app.listen(process.env.PORT||3000, ()=> console.log('Server running & it is public'))
+      app.listen(process.env.PORT||3000, ()=> console.log('Server running & env h'))
     } catch(error) {
       console.log('error connection to MongoDB:', error.message);
     }
